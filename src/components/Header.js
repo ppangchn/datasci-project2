94e88e689b309fdac268cb0d32ce398ab25b9429
Chanissa Trithipkaiwanpon <chanissat@Chanissas-MacBook-Pro-2.local>
2019-04-28 22:34:28 +0700

    update home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+const Container = styled.div`
+    height: 4em;
+    border-bottom: 5px solid #0070A8;
+`
 
 const Header = () => {
-    return (<div>Header</div>)
+    return (<Container>Header</Container>)
 }
 
 export default Header;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import './home.css';
 
 const Home = () => {
-    return (<div>Homeaaaa</div>)
-}
+	return (
+		<div className="home">
+			<div className="d-flex justify-content-center flex-column">
+				<div className="title">Find the best reviewer for your restaurant!</div>
+				<div className="form">sdfsdf</div>
+				<div>
+					<div class="btn btn-white">Clear</div>
+					<div class="btn btn-white">Find</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;

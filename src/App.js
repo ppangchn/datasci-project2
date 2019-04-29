15d70a757101faf34451c24c18b4ac52d6467c88
pong <pongkyman2010@hotmail.com>
2019-04-29 14:10:44 +0700

    reviewerCard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/home';
 import {Route} from 'react-router-dom';
 import './App.css';
 import Reviewer from './pages/reviewer';
+import ReviewerCard from './components/ReviewerCard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <Header />
     <Route exact path="/" component={Home}/>
     <Route exact path="/reviewer" component={Reviewer}/>
+    <Route path='/card' component={ReviewerCard}/>
     </div>
   );
 }
